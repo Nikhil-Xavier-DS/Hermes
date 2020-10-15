6dd9f5ae9fb2b8bd559e5a69247a81834e9615ed
Nikhil Xavier <nikhil.xavier@yahoo.com>
2020-10-15 16:20:30 +0800

    add licence

--- text_classifier/feedforward_attention_model/cnn_attention_model.py
+++ text_classifier/feedforward_attention_model/cnn_attention_model.py
@@ -1,3 +1,20 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""CNN based Feedforward Attention model for text classification."""
+
 import tensorflow as tf
 import time
 import logging
@@ -66,7 +83,9 @@ class FeedForwardAttention(Model):
             for texts, labels in data:
                 with tf.GradientTape() as tape:
                     logits = self.call(texts, training=True)
-                    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits))
+                    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,
+                                                                                         logits=logits,
+                                                                                         label_smoothing=.2,))
                 self.optimizer.lr.assign(self.decay_lr(step))
                 grads = tape.gradient(loss, self.trainable_variables)
                 self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
