d5129dc801259cae1c0291a1b42b3399c09b4d3c
Nikhil Xavier <nikhil.xavier@yahoo.com>
2021-01-13 03:27:43 +0800

    Seq2Seq GRU model

--- semantic_parsing/seq2seq_gru_model/seq2seq.py
+++ semantic_parsing/seq2seq_gru_model/seq2seq.py
@@ -58,27 +58,120 @@ class ProjectedLayer(Layer):
 
 
 class Seq2SeqModel(Model):
-    def __init__(self, units=300, beam_width=12, dropout_rate=0.2):
+    EPOCHS = 1
+    logger = logging.getLogger('tensorflow')
+    logger.setLevel(logging.INFO)
+
+    def __init__(self, lr=1e-4, dropout_rate=0.2, units=300, beam_width=12):
         super().__init__()
         self.embedding = tf.Variable(np.load('../data/embedding.npy'),
                                      dtype=tf.float32,
                                      name='pretrained_embedding',
                                      trainable=False)
         self.encoder = Encoder(units=units, dropout_rate=dropout_rate)
-        self.dropout = Dropout(dropout_rate=dropout_rate)
         self.attention_mechanism = BahdanauAttention(units=units)
         self.decoder_cell = AttentionWrapper(
             GRUCell(units),
             self.attention_mechanism,
             attention_layer_size=units)
         self.projected_layer = ProjectedLayer(self.embed.embedding)
-        self.teach_forcing = BasicDecoder(
+        self.sampler = tfa.seq2seq.sampler.TrainingSampler()
+        self.decoder = BasicDecoder(
             self.decoder_cell,
-            tfa.seq2seq.sampler.TrainingSampler(),
-            output_layer=self.proj_layer)
+            self.sampler,
+            output_layer=self.projected_layer)
         self.beam_search = BeamSearchDecoder(
             self.decoder_cell,
             beam_width=beam_width,
             embedding_fn=lambda x: tf.nn.embedding_lookup(self.embedding, x),
-            output_layer=self.projected_layer,
-            maximum_iterations=80, )
+            output_layer=self.projected_layer)
+        self.optimizer = Adam(lr)
+        self.accuracy = tf.keras.metrics.Accuracy()
+        self.mean = tf.keras.metrics.Mean()
+        self.decay_lr = tf.optimizers.schedules.ExponentialDecay(lr, 1000, 0.95)
+        self.logger = logging.getLogger('tensorflow')
+        self.logger.setLevel(logging.INFO)
+
+    def call(self, inputs, training=True):
+        if training:
+            source, target_in = inputs
+        else:
+            source = inputs
+
+        batch_size = tf.shape(source)[0]
+
+        if source.dtype != tf.int32:
+            source = tf.cast(source, tf.int32)
+
+        source_embedded = tf.nn.embedding_lookup(self.embedding, source)
+        mask = tf.sign(source)
+        encoder_output, encoder_state = self.encoder(source_embedded, mask=mask, training=training)
+
+        if training:
+            target_in_embedded = tf.nn.embedding_lookup(self.embedding, target_in)
+            self.attention_mechanism([encoder_output, tf.math.count_nonzero(source, 1)], setup_memory=True)
+            decoder_initial_state = self.decoder_cell.get_initial_state(batch_size=batch_size,
+                                                                        dtype=tf.float32)
+            decoder_initial_state = decoder_initial_state.clone(cell_state=encoder_state)
+            decoder_output, _, _ = self.decoder(
+                inputs=target_in_embedded,
+                initial_state=decoder_initial_state,
+                sequence_length=tf.math.count_nonzero(target_in, 1, dtype=tf.int32))
+            logits = decoder_output.rnn_output
+        else:
+            encoder_output = tfa.seq2seq.tile_batch(encoder_output, self.beam_width)
+            encoder_seq_len = tfa.seq2seq.tile_batch(tf.math.count_nonzero(source, 1), self.beam_width)
+            encoder_state = tfa.seq2seq.tile_batch(encoder_state, self.beam_width)
+            self.attention_mechanism([encoder_output, encoder_seq_len], setup_memory=True)
+            decoder_initial_state = self.decoder_cell.get_initial_state(batch_size=batch_size * self.beam_width,
+                                                                        dtype=tf.float32)
+            decoder_initial_state = decoder_initial_state.clone(cell_state=encoder_state)
+
+            decoder_output, _, _ = self.beam_search(
+                None,
+                start_tokens=tf.tile(tf.constant([1], tf.int32), [batch_size]),
+                end_token=2,
+                initial_state=decoder_initial_state)
+
+            logits = decoder_output.predicted_ids[:, :, 0]
+
+        return logits
+
+    def fit(self, data, epochs=EPOCHS, vocab_size=9000):
+        t0 = time.time()
+        step = 0
+        epoch = 1
+        while epoch <= epochs:
+            for (source, target_in, target_out) in data:
+                with tf.GradientTape() as tape:
+                    logits = self((source, target_in), training=True)
+                    loss = tf.compat.v1.losses.softmax_cross_entropy(
+                        onehot_labels=tf.one_hot(target_out, (9000+1)),
+                        logits=logits,
+                        weights=tf.cast(tf.sign(target_out), tf.float32),
+                        label_smoothing=.2)
+                self.optimizer.lr.assign(self.decay_lr(step))
+                grads = tape.gradient(loss, self.trainable_variables)
+                grads, _ = tf.clip_by_global_norm(grads, 5.)
+                self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+                if step % 100 == 0:
+                    self.logger.info("Step {} | Loss: {:.4f} | Spent: {:.1f} secs | LR: {:.6f}".format(
+                        step, loss.numpy().item(), time.time() - t0, self.optimizer.lr.numpy().item()))
+                    t0 = time.time()
+                step += 1
+            epoch += 1
+        return True
+
+    def evaluate(self, data):
+        self.mean.reset_states()
+
+        for (source, target_in, target_out) in data:
+            logits = self((source, target_in), training=False)
+
+            for prediction, target in zip(logits.numpy(), target_out.numpy()):
+                matched = np.array_equal(prediction, target)
+                self.mean.update_state(int(matched))
+
+        mean = self.mean.result().numpy()
+        self.logger.info("Evaluation Mean: {:.3f}".format(mean))
+        return True
